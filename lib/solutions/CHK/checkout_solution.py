d7dbd6850146fbc23a447a9db27f72047a393394
sbx_user1051 <sbx_user1051@169.254.215.45>
2022-03-24 20:33:17 +0000

    Thu Mar 24 20:33:17 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -9,21 +9,42 @@ price_table = { 'A': {'Price': 50, 'Offers': ['3A', 130]},
                 'D': {'Price': 15, 'Offers': []}
                }
 
+
 def checkout(skus):
+    # try:
+    if skus.isalpha() or skus == '':
+        order_dict = build_orders(skus)
+        running_total = 0
+        for order in order_dict:
+            cur_total = get_price(order, order_dict[order])
+            running_total += cur_total
+        return running_total
+    else:
+        return -1
+
+
+def get_price(stock, units):
     try:
-        stock = skus[1]
-        units = skus[0]
         item = price_table.get(stock)
         running_total = 0
         offers = item.get('Offers', [])
-        offer = int(item.get('Offers', [])[0][0])
-        if item.get('Offers', []) != []:
+        offer = int(offers[0][0])
+        if offers != []:
             if units >= offer:
                 offer_quantity = int(units / int(offer))
-                running_total += offers[1] * offer_quantity
-                units -= offer_quantity * units
+                running_total += (offers[1] * offer_quantity)
+                units -= int(offer_quantity * offer)
         amount = units * item['Price']
         running_total += amount
         return running_total
     except Exception as e:
-        return -1
+        return 0
+
+
+def build_orders(orders):
+    order_dict = {}
+    for order in orders:
+        quantity = order_dict.get(order.upper(), 0)
+        quantity += 1
+        order_dict.update({order.upper(): quantity})
+    return order_dict
